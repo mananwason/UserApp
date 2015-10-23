f2d3f149c3f38fd2991efe6005ef185409770a2f
mananwason <manan13056@iiitd.ac.in>
2015-10-23 23:53:12 +0530

    Drawer added using NavigationView

---
+++ app/src/main/java/waitr/vendorapp/mc/waitruser/Fragments/MenuFragment.java
@@ -0,0 +1,9 @@
+package waitr.vendorapp.mc.waitruser.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Manan Wason on 23/10/15.
+ */
+public class MenuFragment extends Fragment{
+}
---
+++ app/src/main/java/waitr/vendorapp/mc/waitruser/activities/MainActivity.java
@@ -0,0 +1,117 @@
+package waitr.vendorapp.mc.waitruser.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import waitr.vendorapp.mc.waitruser.Fragments.MenuFragment;
+import waitr.vendorapp.mc.waitruser.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView navigationView;
+    private FrameLayout mainFrame;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpToolbar();
+        setUpNavDrawer();
+        mainFrame = (FrameLayout) findViewById(R.id.layout_main);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, new MenuFragment()).commit();
+
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        setupDrawerContent(navigationView, menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void setUpToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+    }
+
+    private void setUpNavDrawer() {
+        if (mToolbar != null) {
+            final android.support.v7.app.ActionBar ab = getSupportActionBar();
+            assert ab != null;
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+            ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                    mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+            mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setDisplayHomeAsUpEnabled(true);
+            mActionBarDrawerToggle.syncState();
+        }
+    }
+
+    private void setupDrawerContent(NavigationView navigationView, final Menu menu) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        menuItem.setChecked(true);
+                        int id = menuItem.getItemId();
+                        menu.clear();
+                        switch (id) {
+                            case R.id.nav_menu:
+                                fragmentManager.beginTransaction()
+                                        .replace(R.id.content_frame, new MenuFragment()).commit();
+                                getSupportActionBar().setTitle(R.string.menu_items);
+                                break;
+
+                            case R.id.nav_settings:
+
+
+                        }
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+}
